9a284552def670e67595edb200381aa3921559de
YMZ <ymz205042@gmail.com>
2024-09-02 12:55:05 +0800

    ymz:add rnis

--- src/models/components/nis_net.py
+++ src/models/components/nis_net.py
@@ -135,3 +135,72 @@ class NISNet(nn.Module):
                     "count": count}
         
         return x_t1_hat, ei_items
+
+
+class NISPNet(NISNet):
+    def __init__(self, 
+                 input_size: int = 4, 
+                 latent_size: int = 2, 
+                 output_size: int = 4, 
+                 hidden_units: int = 64, 
+                 hidden_units_dyn: int = 64,
+                 is_normalized: bool = True
+                ) -> None:
+        super().__init__(input_size, latent_size, output_size, hidden_units, hidden_units_dyn, is_normalized)
+        
+        self.inv_dynamics = nn.Sequential(
+            nn.Linear(latent_size, self.hidden_units_dyn), 
+            nn.LeakyReLU(), 
+            nn.Linear(self.hidden_units_dyn, self.hidden_units_dyn), 
+            nn.LeakyReLU(), 
+            nn.Linear(self.hidden_units_dyn, latent_size)
+        )
+
+        
+    def back_forward(self, x_t1):
+        h_t1 = self.encoding(x_t1)
+        h_t_hat = self.inv_dynamics(h_t1) + h_t1
+        return h_t_hat
+    
+
+class RNISNet(NISNet):
+    def __init__(self, 
+                 input_size: int = 4, 
+                 latent_size: int = 2, 
+                 output_size: int = 4, 
+                 hidden_units: int = 64, 
+                 hidden_units_dyn: int = 64,
+                 is_normalized: bool = True
+                ) -> None:
+        super().__init__(input_size, latent_size, output_size, hidden_units, hidden_units_dyn, is_normalized)
+
+        self.func = lambda x: (self.dynamics.f(x)[0])
+
+
+        nets_dyn = lambda: nn.Sequential(
+            nn.Linear(latent_size, self.hidden_units_dyn),
+            nn.LeakyReLU(), 
+            nn.Linear(self.hidden_units_dyn, self.hidden_units_dyn), 
+            nn.LeakyReLU(), 
+            nn.Linear(self.hidden_units_dyn, latent_size),
+            nn.Tanh()
+        )
+
+        nett_dyn = lambda: nn.Sequential(
+            nn.Linear(latent_size, self.hidden_units_dyn),
+            nn.LeakyReLU(), 
+            nn.Linear(self.hidden_units_dyn, self.hidden_units_dyn), 
+            nn.LeakyReLU(), 
+            nn.Linear(self.hidden_units_dyn, latent_size)
+        )
+
+        mask1 = torch.cat((torch.zeros(1, latent_size // 2), torch.ones(1, latent_size // 2)), 1)
+        mask2 = 1 - mask1
+        masks_dyn = torch.cat((mask1, mask2, mask1, mask2), 0)
+        self.dynamics = InvertibleNN(nets_dyn, nett_dyn, masks_dyn)
+        
+
+    def back_forward(self, x_t1):
+        h_t1 = self.encoding(x_t1)
+        h_t_hat, _ = self.dynamics.g(h_t1)
+        return h_t_hat
--- src/models/sir_module.py
+++ src/models/sir_module.py
@@ -7,12 +7,18 @@ from torchmetrics import Metric
 from torchmetrics import MaxMetric, MeanMetric
 from torchmetrics import MeanAbsoluteError, MeanMetric, MetricCollection, MinMetric
 from utils.ei import EI
+from sklearn.neighbors import KernelDensity
 # import matplotlib.pyplot as plt
 
 MeanAbsoluteError.__name__ = "MAE"
 
+def kde_density(X):
+    kde = KernelDensity(kernel='gaussian', bandwidth=0.05, atol=0.2).fit(X.cpu().data.numpy()) #bindwidth=0.02
+    log_density = kde.score_samples(X.cpu().data.numpy())
+    return log_density
 
-class SIRLitModule(LightningModule):
+
+class NISLitModule(LightningModule):
     """Example of a `LightningModule` for MNIST classification.
 
     A `LightningModule` implements 8 key methods:
@@ -127,19 +133,6 @@ class SIRLitModule(LightningModule):
         
         return loss, y_hat, y, ei_items
 
-    # def save_and_log_figure(self, h_t, batch_idx):
-
-    #     plt.figure()
-    #     plt.scatter(h_t[:,0].detach().cpu().data.numpy, h_t[:,1].detach().cpu().data.numpy, s=1, alpha=0.25)  # 根据你的数据选择合适的参数
-
-    #     # 保存图片并记录到AIM
-    #     fig_path = f"data/figure_{batch_idx}.png"  # 定义图片保存路径
-    #     plt.savefig(fig_path)
-    #     plt.close()  
-
-    #     # 使用self.log记录图片到AIM
-    #     self.log("train/figure", fig_path, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-
     def training_step(
         self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
     ) -> torch.Tensor:
@@ -154,11 +147,6 @@ class SIRLitModule(LightningModule):
         # h_t = ei_items['h_t']
         # update and log metrics
         self.train_loss(loss)
-        # if batch_idx % 100 == 0:  # 例如，每10个批次生成一次图片
-        #     self.save_and_log_figure(h_t, batch_idx)
-        # self.train_ei(ei_items)
-
-        # ei, ei_term1, ei_term2 = self.train_ei(ei_items)
 
         metrics_dict = self.train_metrics(preds.flatten(), targets.flatten())
         self.log_dict(metrics_dict, on_step=False, on_epoch=True, prog_bar=True)
@@ -277,5 +265,106 @@ class SIRLitModule(LightningModule):
         return {"optimizer": optimizer}
 
 
+class NISpLitModule(NISLitModule):
+    def __init__(self):
+        super().__init__()
+
+        self.temperature = None
+        self.scale = None
+        self.L = None
+        self.weights = torch.ones()
+        self.criterion = torch.nn.L1Loss(reduction='none')
+        self.mae2_w = 1
+
+    def update_weight(self, h_t, L=1):
+        samples = h_t.size(0)
+        scale = h_t.size(1)
+        log_density = kde_density(h_t)
+        log_rho = - scale * np.log(2.0 * L) 
+        logp = log_rho - log_density
+        soft = nn.Softmax(dim=0)
+        weights = soft(torch.tensor(logp))
+        weights = weights * samples
+        self.weights = weights.cuda()
+
+    def reweight(self):
+        h_t_all = self.net.encoding(self.x_t_all)
+        self.update_weight(h_t_all)
+
+    def model_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor]
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Perform a single model step on a batch of data.
+
+        :param batch: A batch of data (a tuple) containing the input tensor of images and target labels.
+
+        :return: A tuple containing (in order):
+            - A tensor of losses.
+            - A tensor of predictions.
+            - A tensor of target labels.
+        """
+        # cal_ei: return h_t, h_t1
+        x, y, w = batch
+        y_hat, ei_items = self.forward(x, y)
+        h_t_hat = self.net.back_forward(y)
+        loss1 = (self.criterion(y_hat, y).mean(axis=1) * w).mean() 
+        loss2 = (self.criterion(h_t_hat, ei_items['h_t']).mean(axis=1) * w).mean()
+        loss = loss1 + self.mae2_w * loss2
+        return loss, y_hat, y, ei_items
+    
+    def training_step(
+            self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+        ) -> torch.Tensor:
+
+            loss, preds, targets, ei_items = self.model_step(batch)
+            h_t = ei_items['h_t']
+
+            self.train_loss(loss)
+
+            metrics_dict = self.train_metrics(preds.flatten(), targets.flatten())
+            self.log_dict(metrics_dict, on_step=False, on_epoch=True, prog_bar=True)
+            self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
+
+            # self.log("train/ei", self.train_ei, on_step=True, on_epoch=True, prog_bar=True)
+
+            # self.log("train/ei", ei, on_step=True, on_epoch=True, prog_bar=True)
+            # self.log("train/ei_term1", ei_term1, on_step=True, on_epoch=True, prog_bar=True)
+            # self.log("train/ei_term2", ei_term2, on_step=True, on_epoch=True, prog_bar=True)
+
+
+            # return loss or backpropagation will fail
+            return loss
+
+    def train_step2(self, mae2_w, batch_size):
+        self.net.train()
+        start = np.random.randint(self.samp_num - batch_size)
+        end = start + batch_size
+        x_t, x_t1, w = self.x_t_all[start:end], self.x_t1_all[start:end], self.weights[start:end]
+        x_t1_hat, ei_items = self.net(x_t, x_t1)
+        h_t_hat = self.net.back_forward(x_t1)
+        mae1 = (self.MAE_raw(x_t1, x_t1_hat).mean(axis=1) * w).mean() 
+        mae2 = (self.MAE_raw(h_t_hat, ei_items['h_t']).mean(axis=1) * w).mean() 
+        loss = mae1 + mae2_w * mae2
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    def training(self, T1, T_all, mae2_w, batch_size, clip=200):
+        for epoch in range(T_all):
+            if epoch < T1:
+                self.train_loss += self.train_step(batch_size)
+            else:
+                self.train_loss += self.train_step2(mae2_w, batch_size)
+            if epoch % clip == 0:
+                self.test_loss, dei, term1, term2 = self.test_step()
+                self.train_loss /= clip
+                self.log(dei, term1, term2, epoch)
+                self.train_loss = 0
+            if epoch > T1 and epoch % 1000 == 0:
+                self.reweight()
+                print(self.weights[:10])
+
+
 if __name__ == "__main__":
-    _ = SIRLitModule(None, None, None, None)
+    _ = NISLitModule(None, None, None, None)
